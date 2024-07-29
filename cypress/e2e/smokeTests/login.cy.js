9a69093c8da03b082aeb969a0c8680df6c88de5c
rajusigmasolve <raju@sigmasolve.net>
2024-07-29 12:29:56 +0530

    hooks

--- cypress/e2e/smokeTests/login.cy.js
+++ cypress/e2e/smokeTests/login.cy.js
@@ -1,14 +1,26 @@
 
+
 import data from "../../fixtures/logindata.json"
 describe('Verify Login Functionality', () => {
 
     const ussername = "Admin"
     const password = "admin123"
 
-
-    it("Verify login with Valid Credentials", () => {
+    beforeEach("Launch url", ()=>{
+            
+        cy.log("Test Execution Started")
 
         cy.visit("/web/index.php/auth/login")
+
+    })
+
+    afterEach( ()=>{
+        
+        cy.log("Test Execution completed")
+
+    })
+
+    it("Verify login with Valid Credentials", () => {
 
         cy.get("input[name='username']").type(ussername)
 
@@ -24,8 +36,7 @@ describe('Verify Login Functionality', () => {
     it('Verify login with valid username and invalid password', () => {
 
         let invalidpassword = "ergnrek"
-        cy.visit("/web/index.php/auth/login")
-
+  
         cy.get('input[name="username"]').type(data.username)
 
         cy.get('#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div:nth-child(3) > div > div:nth-child(2) > input').type(invalidpassword)
@@ -35,9 +46,8 @@ describe('Verify Login Functionality', () => {
         cy.contains('Invalid credentials').should("be.visible")
     })
 
-    it('Verify login with invalid username and valid password', () => {
+    it.skip('Verify login with invalid username and valid password', () => {
 
-        cy.visit("/web/index.php/auth/login")
 
         cy.get('input[name="username"]').type(data.wronmgusername)
 
@@ -49,17 +59,17 @@ describe('Verify Login Functionality', () => {
     })
  
 
-    it('Verify login with invalid username and invalid password', () => {
+    specify('Verify login with invalid username and invalid password', () => {
 
         //cy.viewport(375,667)
 
         cy.viewport("macbook-16")
 
-        cy.visit("/web/index.php/auth/login")
+  
 
         cy.get("#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div:nth-child(2) > div > div:nth-child(2) > input").type(data.wronmgusername)
 
-        cy.get('#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div:nth-child(3) > div > div:nth-child(2) > input').type(invalidpassword)
+        cy.get('#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div:nth-child(3) > div > div:nth-child(2) > input').type("invalidpassword")
 
         cy.get("#app > div.orangehrm-login-layout > div > div.orangehrm-login-container > div > div.orangehrm-login-slot > div.orangehrm-login-form > form > div.oxd-form-actions.orangehrm-login-action > button").click()
       
@@ -67,10 +77,10 @@ describe('Verify Login Functionality', () => {
     })
 
 
-    it("without entering username and password", ()=>{
-        cy.visit("/web/index.php/auth/login")
+    it.skip("without entering username and password", ()=>{
+      
         cy.get('button[type="submit"]').click()
-        cy.contains('Invalid credentials').should("be.visible")
+        cy.contains('Required').should("be.visible")
 
     })
   })
